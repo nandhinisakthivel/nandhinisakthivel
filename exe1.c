e9c89a7ff76e8eb371efc5299d7b0efe37956fba
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-01-19 14:44:00 +0530

    Update exe1.c

--- exe1.c
+++ exe1.c
@@ -1,1 +1,23 @@
-tyie4yt
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    if(n<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+    getch();
+    
+}
+
