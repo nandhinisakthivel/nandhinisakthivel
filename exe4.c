5e2f3a354b286c471c1f80ba78cc960a868fb37d
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-01-25 22:57:55 +0530

    Rename exe3.c to exe4.c

---
+++ exe4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("Enter a");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    getch();
+}
