2188e6f2e37a5cfd418d0e3ab4343423de9a080c
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-01-27 21:52:52 +0530

    Create exe6.c

---
+++ exe6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char n;
+    printf("Enter n");
+    scanf("%d",&n);
+    if(n>=a&&n>=z||n>=A&&n>=Z)
+    {
+        printf("alphabet");
+    }
+    else
+    {
+        printf("not");
+    }
+    getch();
+}
