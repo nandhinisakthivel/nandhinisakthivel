a0a0260f45ff7e068e41ad4c3bb514f281a9e06d
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-01-27 21:38:17 +0530

    Create exe5.c

---
+++ exe5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    printf("enter choice");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case 'a':
+        printf("vowel");
+        break;
+        case 'e':
+        printf("vowel");
+        break;
+        case 'i':
+        printf("vowel");
+        break;
+        case 'o':
+        printf("vowel");
+        break;
+        case 'u':
+        printf("vowel");
+        break;
+        default:
+        printf("constant");
+        
+    }
+    getch();
+}
