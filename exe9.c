076bc06630bbd00f08f084d6e2328355b85d568e
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-01-28 20:16:23 +0530

    Create exe9.c

---
+++ exe9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    printf("enter n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("Hello\n");
+    }
+    getch();
+}
