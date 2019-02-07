e2cc69908464108a43580d56bd1cc1fe6cb3e445
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-02-07 20:54:10 +0530

    Create s7.c

---
+++ s7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+     int n,m;
+    scanf("%d %d",&n,&m);
+    n=n+m;
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
