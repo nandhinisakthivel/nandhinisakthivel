fa05df5ce6d35b0463f6e8a5b0d565bb9276e299
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-02-07 21:39:18 +0530

    Update s72.c

--- s72.c
+++ s72.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
     char a[50];
     int i,n=0,c=0;
@@ -20,5 +21,5 @@ int main()
     {
         printf("no");
     }
-return 0;
+getch();
 }
