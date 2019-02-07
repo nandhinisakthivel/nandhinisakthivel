c698b5f265fb97ade8c78eaf8a94b399a43b6b44
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-02-07 21:43:16 +0530

    Create s73.c

---
+++ s73.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],i,min;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<10;i++)
+    {
+        if(min>=a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("%d",min);
+
+getch();
+}
