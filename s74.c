e3486fc1f1e9b6dd85cdfc910876f0a4c4244d34
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-02-07 21:47:11 +0530

    Create s74.c

---
+++ s74.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,k,s;
+    scanf("%d %d",&n,&k);
+    s=n+k;
+    if(s%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+getch();
+
+}
