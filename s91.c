8a53f52c46e717a36f5924be7cce44c695ba18e5
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-02-08 19:19:24 +0530

    Create s91.c

---
+++ s91.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int kabali,opponent;
+    scanf("%d %d",&kabali,&opponent);
+    if(kabali<opponent)
+    {
+        kabali=opponent-kabali;
+    }
+    else
+    {
+     
+     kabali=kabali-opponent;
+    
+    }
+    printf("%d",kabali);
+
+    return 0;
+    
+}
