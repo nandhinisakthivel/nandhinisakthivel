b4b6f3528c7fb2aaae8160022c2d5f28c0f4edf4
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-03-10 20:41:39 +0530

    Update play101.c

--- play101.c
+++ play101.c
@@ -1,14 +1,16 @@
-
 int main()
+
 {
    long int a,bin=0,rem,p=1;
     scanf("%ld",&a);
     while(a>0)
+    
     {
         rem=a%2;
         bin=bin+rem*p;
         p=p*10;
         a=a/2;
+    
     }
     printf("%ld",bin);
     return 0;
