3c83a6661891383e3575bc2aaf26dfa2f3cdbb01
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-03-24 21:28:06 +0530

    Update play168.c

--- play168.c
+++ play168.c
@@ -1,16 +1,22 @@
 int main(void) 
 {
 	int n,m,i,j,rem,pro,a[20],count=0,count1=0,temp,pos=0;
+	
 	scanf("%d %d",&n,&m);
 	pro=n*m;
+	
 	while(pro!=0)
+	
 	{
 		rem=pro%2;
+	
 		a[i]=rem;
 		i++;
+		
 		count=count+1;
 		pro=pro/2;
 	}
+	
 	
 	i=0;j=count-1;
 	while(i<j)
@@ -25,14 +31,22 @@ int main(void)
 	{
 		pos++;
 		if(a[i]==1)
+	
 		{
 			count1++;
 		}
+		
 		if(count1==2)
 		{
+		
 			printf("%d",pos);
+			
 			break;
 		}
+		
 	}
+	
+	
 	return 0;
 }
+
