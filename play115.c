a3b899192378234ccfb1aa083703964973cf7d44
nandhinisakthivel <46595996+nandhinisakthivel@users.noreply.github.com>
2019-04-03 16:07:44 +0530

    Update play115.c

--- play115.c
+++ play115.c
@@ -1,24 +1,58 @@
-#include <stdio.h>
-
-int main() 
+#include<stdio.h>
+void main()
 {
-	int a[10],n,sum=0,i,j,temp;
+ int n,a[100000],b[1000000],i,j,k;
+    printf("enter n");
 	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		b[i]=a[i];
+	}
+
     for(i=0;i<n;i++)
+
     {
-        scanf("%d",&a[i]);
+
+        for(j=i+1;j<n;j++)
+
+        {
+
+            if(a[i]>a[j])
+
+            {
+
+                k=a[j];
+
+                a[j]=a[i];
+
+                a[i]=k;
+
+            }
+
+        }
+
     }
-   for(i=0;i<n;i++)
-   {
-       for(j=i+1;j<n;j++)
-       {
-           if(a[i]>a[j])
-           {
-               temp=a[i];
-               a[i]=a[j];
-               a[j]=temp;
-           }
-       }
-        printf("%d ",i+1);
-   }
-  	return 0;
+
+    for(i=0;i<n;i++)
+    
+
+    {
+
+        for(j=0;j<n;j++)
+
+        {
+
+            if(a[i]==b[j])
+
+            {
+
+                printf("%d ",j+1);
+
+            }
+
+        }
+
+            }
+            
+}
